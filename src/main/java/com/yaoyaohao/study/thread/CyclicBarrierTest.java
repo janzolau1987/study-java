feeee37445b0a716a5f0f3ad47a59625e16ad56c
janzolau1987 <janzolau1987@gmail.com>
2016-08-03 15:22:08 +0800

    CyclicBarrier与CountDownLatch区别

--- src/main/java/com/yaoyaohao/study/thread/CyclicBarrierTest.java
+++ src/main/java/com/yaoyaohao/study/thread/CyclicBarrierTest.java
@@ -5,6 +5,10 @@ import java.util.concurrent.CyclicBarrier;
 
 /**
  * CyclicBarrier同步屏障示例
+ * 	
+ * 	CyclicBarrier与CountDownLatch区别：
+ * 	1、CountDownLatch的计数器只能使用一次，而CyclicBarrier的计数器可以使用reset()方法重置
+ * 	2、CyclicBarrier还提供其他有用的方法，如getNumberWaiting方法可以获得阻塞的线程数量。isBroken()方法用来了解阻塞的线程是否被中断
  * 
  * @author liujianzhu
  * @date 2016年8月3日 下午3:03:48

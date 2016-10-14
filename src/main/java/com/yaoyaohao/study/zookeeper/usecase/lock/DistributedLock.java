8b7663002c91c53d73da65d0aeaf70ed70d2ec4f
janzolau1987 <janzolau1987@gmail.com>
2016-10-14 17:54:02 +0800

    ZooKeeper典型应用场景  之 分布式Lock

---
+++ src/main/java/com/yaoyaohao/study/zookeeper/usecase/lock/DistributedLock.java
@@ -0,0 +1,22 @@
+package com.yaoyaohao.study.zookeeper.usecase.lock;
+
+/**
+ * 分布式锁定义
+ * 
+ * @author liujianzhu
+ * @date 2016年10月14日 下午5:01:11
+ */
+public interface DistributedLock {
+	/**
+	 * 获取锁，若未成功获取得等待,直到成功获取
+	 * @throws Exception
+	 */
+	public void acquire() throws Exception;
+
+
+	/**
+	 * 锁释放
+	 * @throws Exception
+	 */
+	public void release() throws Exception;
+}

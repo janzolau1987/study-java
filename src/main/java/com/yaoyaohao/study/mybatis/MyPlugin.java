6f144cbe83dc8f9bfa05d313ac279d052708ab87
janzolau1987 <janzolau1987@gmail.com>
2017-06-20 15:11:18 +0800

    使用MetaObject工具类操作代理对象

--- src/main/java/com/yaoyaohao/study/mybatis/MyPlugin.java
+++ src/main/java/com/yaoyaohao/study/mybatis/MyPlugin.java
@@ -1,16 +1,19 @@
 package com.yaoyaohao.study.mybatis;
 
-import java.sql.Connection;
 import java.sql.Statement;
 import java.util.Properties;
 
 import org.apache.ibatis.executor.statement.StatementHandler;
+import org.apache.ibatis.mapping.BoundSql;
 import org.apache.ibatis.plugin.Interceptor;
 import org.apache.ibatis.plugin.Intercepts;
 import org.apache.ibatis.plugin.Invocation;
 import org.apache.ibatis.plugin.Plugin;
 import org.apache.ibatis.plugin.Signature;
+import org.apache.ibatis.reflection.MetaObject;
+import org.apache.ibatis.reflection.SystemMetaObject;
 import org.apache.ibatis.session.ResultHandler;
+import org.springframework.cglib.proxy.Proxy;
 
 /**
  * 拦截sql执行，统计时间
@@ -27,9 +30,18 @@ public class MyPlugin implements Interceptor {
 
 	@Override
 	public Object intercept(Invocation invocation) throws Throwable {
+		Object target = invocation.getTarget();
+		while(Proxy.isProxyClass(target.getClass())) {
+			MetaObject metaObject = SystemMetaObject.forObject(target);
+			target = metaObject.getValue("h.target");
+		}
+		MetaObject metaStatementHandler = SystemMetaObject.forObject(target);
+		BoundSql boundSql = (BoundSql) metaStatementHandler.getValue("delegate.boundSql");
+		System.out.println("执行SQL : "+boundSql.getSql());
+		//
 		long beginTime = System.currentTimeMillis();
 		Object result = invocation.proceed();
-		System.out.println("执行时间：" + (System.currentTimeMillis() - beginTime) + " ms");
+		System.out.println("执行时间 : " + (System.currentTimeMillis() - beginTime) + " ms");
 		return result;
 	}
 
